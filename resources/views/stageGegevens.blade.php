9b89b1801723a32ee2f0060c3e561a36c21eb099
haitamattar <haitam@live.nl>
2016-03-03 23:14:24 +0100

    stagegegevens pagina

---
+++ resources/views/stageGegevens.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('headerImage')
+	Stage Gegevens
+@stop
+
+@section('content')
+	<div class='container-center center-project'>
+		<div id='werkprocessContent'>
+			Bedrijf: idd.nl/ii.nl<br>
+			straat: koornmarkt 34
+	</div>
+@stop
